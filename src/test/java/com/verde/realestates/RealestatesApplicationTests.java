cba0e5c73e078e2164247a38b5362f5daf59edb5
amirparvaneh <amirparvaneh138@gmail.com>
2024-06-06 14:02:26 +0330

    dev-1

---
+++ src/test/java/com/verde/realestates/RealestatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verde.realestates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
